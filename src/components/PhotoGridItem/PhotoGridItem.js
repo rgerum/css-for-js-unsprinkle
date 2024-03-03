6f183a00a34668e0ef8fe20fd05cdeaa5412f902
Richard Gerum <14153051+rgerum@users.noreply.github.com>
2024-03-03 11:38:03 -0500

    added tag ellipsis

--- src/components/PhotoGridItem/PhotoGridItem.js
+++ src/components/PhotoGridItem/PhotoGridItem.js
@@ -52,17 +52,22 @@ const Image = styled.img`
 `;
 
 const Tags = styled.ul`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 8px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    white-space: nowrap;
+    padding: 8px 0;
 `;
 
 const Tag = styled.li`
+    display: inline;
+    margin-right: 8px;
   padding: 4px 8px;
   background: var(--color-gray-300);
   font-size: 0.875rem;
   font-weight: 475;
   color: var(--color-gray-800);
+    
 `;
 
 export default PhotoGridItem;
